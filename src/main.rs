0bfac863bc5d56b2ed9c7854627ab74f6027f57b
杉浦寛行 <r@rR.local>
2022-07-03 13:18:05 +0900

    add colorscheme 'enfocado' & 'iceberg'

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use {
 
 fn main() {
    let path = std::env::args().nth(1,).unwrap_or("~/.config/nvim/init.vim".to_string(),);
-   let colors = vec!["PaperColor", "flatui", "nova"];
+   let colors = vec!["PaperColor", "flatui", "nova", "enfocado", "iceberg"];
    let color = colors[thread_rng().gen_range(0..colors.len(),)];
    let colo = format!("-c color {}", color);
    Command::new("nvim",).args([&colo, &path,],).exec();
